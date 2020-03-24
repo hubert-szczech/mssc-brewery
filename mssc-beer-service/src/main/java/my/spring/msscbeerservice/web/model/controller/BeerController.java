ccd1d14b33b39c7180ffbc9a8f50ff36a2fb036f
hszczech <hszczech@sollan.com>
2020-03-24 22:45:04 +0100

    controller

---
+++ mssc-beer-service/src/main/java/my/spring/msscbeerservice/web/model/controller/BeerController.java
@@ -0,0 +1,32 @@
+package my.spring.msscbeerservice.web.model.controller;
+
+import my.spring.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId")UUID beerID){
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beer){
+        //todo impl
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId")UUID beerID,@RequestBody BeerDto beer ){
+        //todo impl
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
